0be074f682eb6894df396ab4cff3df6a97034a12
Marco Antonio Juarez Miranda <marcojm28@hotmail.com>
2019-02-03 20:57:58 -0500

    Servicio REST FourSquare

---
+++ TravelRecordsApp/TravelRecordsApp/Helper/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecordsApp.Helper
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = "4MUZNJ5WPQWTFRV0HTHYOEA5KOHY2Y5TW31DUBX0RNLBWTYU";
+        public const string CLIENT_SECRET = "INTZXQYIZUBCBEWFWOI5DGAFAJIYOWFN0GUA02GLOIXYG2W3";
+
+
+    }
+}
--- TravelRecordsApp/TravelRecordsApp/NewTravelPage.xaml.cs
+++ TravelRecordsApp/TravelRecordsApp/NewTravelPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using TravelRecordsApp.Model;
+using Plugin.Geolocator;
+using TravelRecordsApp.Logic;
 
 namespace TravelRecordsApp
 {
@@ -16,6 +18,19 @@ namespace TravelRecordsApp
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var locator = CrossGeolocator.Current;
+
+            var position = await locator.GetPositionAsync();
+
+            var venue = await VenueLogic.GetVenues(position.Latitude,position.Longitude);
+
+            ListViewVenue.ItemsSource = venue;
         }
 
         private void ToolBarGuardar_Clicked(object sender, EventArgs e)
